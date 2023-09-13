2250fa32ea196fb9c96fa558ad62beae09e9e940
KhodeMamalizz <mohammad.hosein.ghorbani82@gmail.com>
2023-09-13 18:53:58 +0330

    fixed miss spelled url

--- api/useCallApi.ts
+++ api/useCallApi.ts
@@ -12,7 +12,7 @@ export const useCallApi = () => {
         currentLoading.value = true
         return new Promise((resolve: any, reject: any) => {
             try {           
-                useLazyFetch<any>(`http://api.weatherapi.com/v1/current.json?key=${config.public.API_KEY}&q=${location}&aqi=yes`).then((response: any) => {
+                useLazyFetch<any>(`https://api.weatherapi.com/v1/current.json?key=${config.public.API_KEY}&q=${location}&aqi=yes`).then((response: any) => {
                     const { data } = response
                     setHomeCurrentData(toRaw(data.value))
                     currentLoading.value = false
@@ -29,7 +29,7 @@ export const useCallApi = () => {
         forecastLoading.value = true
         return new Promise((resolve: any, reject: any) => {
             try {           
-                useLazyFetch<any>(`http://api.weatherapi.com/v1/forecast.json?key=${config.public.API_KEY}&q=${location}&days=1&aqi=no&alerts=no`).then((response) => {
+                useLazyFetch<any>(`https://api.weatherapi.com/v1/forecast.json?key=${config.public.API_KEY}&q=${location}&days=1&aqi=no&alerts=no`).then((response) => {
                     const { data } = response
                     setHomeForecastData(toRaw(data.value))
                     forecastLoading.value = false
@@ -46,7 +46,7 @@ export const useCallApi = () => {
         searchLoading.value = true
         return new Promise((resolve: any, reject: any) => {
             try {           
-                useLazyFetch<any>(`http://api.weatherapi.com/v1/search.json?key=${config.public.API_KEY}&q=${query}`).then((response) => {
+                useLazyFetch<any>(`https://api.weatherapi.com/v1/search.json?key=${config.public.API_KEY}&q=${query}`).then((response) => {
                     const { data } = response
                     setHomeSearchData(toRaw(data.value))
                     searchLoading.value = false
